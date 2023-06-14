5fbe969098668d44b01f71515a1cedd0b2863732
Nurlybek_S <nurlybekqwerty@gmail.com>
2023-06-14 23:27:06 +0600

    add component

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Counter from "./component/Counter/Counter";
+import { Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/element" element={<Counter />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
