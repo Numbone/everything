06d5ba845a3ff762402635b7fea66aa75b6a0a38
Nurlybek_S <nurlybekqwerty@gmail.com>
2023-07-04 15:30:13 +0600

    testing CI pipeline

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -7,7 +7,7 @@ import { BuildOptions } from './types/config';
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 export function buildPlugins({ paths, isDev }:BuildOptions):webpack.WebpackPluginInstance[] {
-    return [
+    const plugins = [
         new HtmlWebpackPlugin({
             template: paths.html,
         }),
@@ -16,15 +16,17 @@ export function buildPlugins({ paths, isDev }:BuildOptions):webpack.WebpackPlugi
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: 'css/[name].[contenthash:8].css',
         }),
-        new webpack.DefinePlugin(
-            {
-                __IS_DEV__: JSON.stringify(isDev),
-            },
-        ),
-
-        new webpack.HotModuleReplacementPlugin(),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false,
+        new webpack.DefinePlugin({
+            __IS_DEV__: JSON.stringify(isDev),
         }),
     ];
+
+    if (isDev) {
+        plugins.push(new webpack.HotModuleReplacementPlugin());
+        plugins.push(new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }));
+    }
+
+    return plugins;
 }
