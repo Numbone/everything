8eb95f2f2fa7d3d6d4be8a5cfa2306c7a473e35c
Nurlybek_S <nurlybekqwerty@gmail.com>
2023-09-04 18:39:33 +0600

    add article dynamic module

--- src/entities/Article/index.ts
+++ src/entities/Article/index.ts
@@ -1,5 +1,7 @@
+import { ArticleDetailsSchema } from './module/types/articleDetailsSchema';
 import { Article } from './module/types/article';
 import { ArticleDetails } from './ui/ArticleDetails/ArticleDetails';
 
 export { ArticleDetails };
 export type { Article };
+export { ArticleDetailsSchema };
--- src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/className';
+import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import { articleDetailsReducer } from '../../module/slice/articleDetailsSlice';
 import styles from './ArticleDetails.module.scss';
 
 interface Props {
     className?: string
 }
+const reducers :ReducersList = {
+    articleDetails: articleDetailsReducer,
+};
 const ArticleDetails = (props:Props) => {
     const { className } = props;
     const { t } = useTranslation();
     return (
-        <div className={classNames(styles.content, {}, [className])}>
-            1233
-        </div>
+        <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
+            <div className={classNames(styles.content, {}, [className])}>
+                1233
+            </div>
+        </DynamicModuleLoader>
+
     );
 };
 
